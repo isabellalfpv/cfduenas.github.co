0a1b3db556309be1875059cc6342f262a03c202f
isabellalfpv <122456697+isabellalfpv@users.noreply.github.com>
2023-01-11 08:43:45 -0500

    Add files via upload

---
+++ edition.cs
@@ -0,0 +1,13 @@
+body { 
+ background: lightpurple;
+}
+h1{
+    font-weight: 600;
+    font-family: "Arial";
+    text-align: center;
+    text-decoration:underline;
+}
+h2
+    font-family: "Arial" ;
+    text-decoration:underline;
+}
